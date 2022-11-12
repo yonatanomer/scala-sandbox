cded5430e033ce586cd45a45c4f7eb6718721820
yonatanomer <yonathan.omer@budgetbakers.com>
2022-11-12 17:50:25 +0100

    kafka basics

--- case-studies/kafka_demo/src/main/scala/kafka_test/ProducerApp.scala
+++ case-studies/kafka_demo/src/main/scala/kafka_test/ProducerApp.scala
@@ -1,45 +1,14 @@
 package kafka_test
 
 import cats.effect.{ExitCode, IO, IOApp}
-import com.yon.kafka.KafkaClientConfig._
-import com.yon.kafka.ProducerFactory._
+import com.yon.kafka.MessageProducer
 import io.circe.generic.auto._
 import kafka_test.CarTrafficDummyData._
-import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerRecord, RecordMetadata}
-
-import scala.collection.immutable.Seq
-import scala.concurrent.Promise
-import scala.concurrent.duration.DurationInt
 
 object ProducerApp extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    producer[CarId, CarSpeed]("json-topics")
-      .use(produce("car-speed", carSpeed))
+    MessageProducer[CarId, CarSpeed]("json-topics")
+      .use(_.produce("car-speed", carSpeed))
       .as(ExitCode.Success)
   }
-
-  def produce[K, V](topic: String, records: Seq[(K, V)])(producer: KafkaProducer[K, V]): IO[Nothing] = {
-    IO(send(producer)(topic, records)).foreverM
-  }
-
-  private def send[K, V](
-      producer: KafkaProducer[K, V]
-  )(topic: String, records: Seq[(K, V)]): Seq[IO[Unit]] = {
-    records.map { case (k, v) =>
-      Thread.sleep(1000)
-
-      val p = Promise[Unit]()
-      producer.send(
-        new ProducerRecord[K, V](topic, k, v),
-        new Callback {
-          override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
-            println("produced a record")
-            Option(exception).map(p.failure).getOrElse(p.success(()))
-          }
-        }
-      )
-      IO.fromFuture(IO(p.future)) *> IO(println(s"produced data to [$topic]")) *> IO.sleep(2.seconds)
-    }
-  }
-
 }
--- common/src/main/scala/com/yon/kafka/ProducerFactory.scala
+++ common/src/main/scala/com/yon/kafka/ProducerFactory.scala
@@ -4,19 +4,53 @@ import cats.effect.{IO, Resource}
 import com.yon.kafka.KafkaClientConfig.producerProps
 import com.yon.kafka.Serialization.serializer
 import io.circe.Encoder
-import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.{Callback, KafkaProducer, ProducerRecord, RecordMetadata}
 
+import scala.collection.immutable.Seq
+import scala.concurrent.Promise
+import scala.concurrent.duration.DurationInt
 import scala.jdk.CollectionConverters._
 
-object ProducerFactory {
-  def producer[K >: Null, V >: Null](clientId: String)(implicit
+class MessageProducer[K >: Null, V >: Null](kafkaProducer: KafkaProducer[K, V]) {
+
+  def produce(topic: String, records: Seq[(K, V)]): IO[Nothing] = {
+    IO(send(topic, records)).foreverM
+  }
+
+  private def send(topic: String, records: Seq[(K, V)]): Seq[IO[Unit]] = {
+    records.map { case (k, v) =>
+      Thread.sleep(1000)
+
+      val p = Promise[Unit]()
+      kafkaProducer.send(
+        new ProducerRecord[K, V](topic, k, v),
+        new Callback {
+          override def onCompletion(metadata: RecordMetadata, exception: Exception): Unit = {
+            println("produced a record")
+            Option(exception).map(p.failure).getOrElse(p.success(()))
+          }
+        }
+      )
+      IO.fromFuture(IO(p.future)) *> IO(println(s"produced data to [$topic]")) *> IO.sleep(2.seconds)
+    }
+  }
+
+  def close(): IO[Unit] = IO(kafkaProducer.close())
+}
+
+object MessageProducer {
+  def apply[K >: Null, V >: Null](clientId: String)(implicit
       keyEncoder: Encoder[K],
       valEncoder: Encoder[V]
-  ): Resource[IO, KafkaProducer[K, V]] = {
-    Resource
-      .make(IO(new KafkaProducer[K, V](producerProps(clientId).asJava, serializer[K], serializer[V])))(p => {
-        IO(p.close())
-      })
+  ): Resource[IO, MessageProducer[K, V]] = {
+    Resource.make(IO {
+      val kafkaProducer = new KafkaProducer[K, V](
+        producerProps(clientId).asJava,
+        serializer[K],
+        serializer[V]
+      )
+      new MessageProducer(kafkaProducer)
+    })(_.close())
   }
 
 }
