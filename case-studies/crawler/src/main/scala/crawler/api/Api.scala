099860e94b4a2b9d92fcde0ccafa30b96c1ee66e
yonatanomer <yonathan.omer@budgetbakers.com>
2022-11-23 19:58:28 +0100

    cake pattern

--- case-studies/crawler/src/main/scala/crawler/api/Api.scala
+++ case-studies/crawler/src/main/scala/crawler/api/Api.scala
@@ -1,9 +1,14 @@
 package crawler.api
 
 import cats.effect.IO
+import com.yon.kafka.CakeMessageProducer
 import crawler.{AppContext, CrawlerApp}
 import crawler.api.Schema.{BasicServerEndpoint, ErrorOut, Output, crawlEndpointSchema}
+import crawler.handlers.CrawlRequestHandler
+import crawler.persistance.{CrawlTask, MongoTasksDao}
+import org.http4s.HttpRoutes
 import sttp.tapir.server.ServerEndpoint
+import sttp.tapir.server.http4s.Http4sServerInterpreter
 
 object Api {
   def crawlEndpoint(app: CrawlerApp): BasicServerEndpoint[CrawlParams, Output, ErrorOut] = {
@@ -12,5 +17,18 @@ object Api {
 
   def endpoints(app: CrawlerApp): List[ServerEndpoint[Any, IO]] =
     List(crawlEndpoint(app))
+}
 
+trait Routes {
+  this: CrawlRequestHandler =>
+  def crawlEndpoint(app: CrawlerApp): BasicServerEndpoint[CrawlParams, Output, ErrorOut] = {
+    crawlEndpointSchema.serverLogic(crawl(_))
+  }
+
+  def endpoints(app: CrawlerApp): List[ServerEndpoint[Any, IO]] =
+    List(crawlEndpoint(app))
+
+  def initRoutes(app: CrawlerApp): HttpRoutes[IO] = {
+    Http4sServerInterpreter[IO]().toRoutes(Api.endpoints(app) ++ Schema.docs())
+  }
 }
